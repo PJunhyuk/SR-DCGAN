c71fa6b0db9931c8b91a192bd4b06685058fdc8e
PJunhyuk <jgravity@naver.com>
2016-08-28 01:34:15 +0900

    test

--- train.lua
+++ train.lua
@@ -51,6 +51,17 @@ end
 
 
 
+local function weights_init(m)
+   local name = torch.type(m)
+   if name:find('Convolution') then
+      m.weight:normal(0.0, 0.02)
+      m:noBias()
+   elseif name:find('BatchNormalization') then
+      if m.weight then m.weight:normal(1.0, 0.02) end
+      if m.bias then m.bias:fill(0) end
+   end
+end
+
 local ngf = 64
 local ndf = 64
 local nz = 100
