0ca7f437500791968d7e236125166a3762d5133e
PJunhyuk <jgravity@naver.com>
2016-08-28 01:29:53 +0900

    Test nn

--- train.lua
+++ train.lua
@@ -26,6 +26,8 @@ local alpha_util = require 'alpha_util'
 
 
 
+require 'nn'
+
 
 
 local image_list = './data/image_list.txt'
@@ -37,10 +39,56 @@ for i = 1, #csv do ------ #csv : 9999
   local img = Image.load(filename, 3, 'byte')
   img = Image.rgb2y(img)
   img = Image.scale(img, 64, 64)
-
   table.insert(x, {compression.compress(img), {data = {filters = filters}}})
   xlua.progress(i, #csv)
   if i % 10 == 0 then
     collectgarbage()
   end
 end
+
+local netG = nn.Sequential()
+-- input is Z, going into a convolution
+netG:add(SpatialFullConvolution(nz, ngf * 8, 4, 4))
+netG:add(SpatialBatchNormalization(ngf * 8)):add(nn.ReLU(true))
+-- state size: (ngf*8) x 4 x 4
+netG:add(SpatialFullConvolution(ngf * 8, ngf * 4, 4, 4, 2, 2, 1, 1))
+netG:add(SpatialBatchNormalization(ngf * 4)):add(nn.ReLU(true))
+-- state size: (ngf*4) x 8 x 8
+netG:add(SpatialFullConvolution(ngf * 4, ngf * 2, 4, 4, 2, 2, 1, 1))
+netG:add(SpatialBatchNormalization(ngf * 2)):add(nn.ReLU(true))
+-- state size: (ngf*2) x 16 x 16
+netG:add(SpatialFullConvolution(ngf * 2, ngf, 4, 4, 2, 2, 1, 1))
+netG:add(SpatialBatchNormalization(ngf)):add(nn.ReLU(true))
+-- state size: (ngf) x 32 x 32
+netG:add(SpatialFullConvolution(ngf, nc, 4, 4, 2, 2, 1, 1))
+netG:add(nn.Tanh())
+-- state size: (nc) x 64 x 64
+
+netG:apply(weights_init)
+
+print ("netG end")
+
+local netD = nn.Sequential()
+
+-- input is (nc) x 64 x 64
+netD:add(SpatialConvolution(nc, ndf, 4, 4, 2, 2, 1, 1))
+netD:add(nn.LeakyReLU(0.2, true))
+-- state size: (ndf) x 32 x 32
+netD:add(SpatialConvolution(ndf, ndf * 2, 4, 4, 2, 2, 1, 1))
+netD:add(SpatialBatchNormalization(ndf * 2)):add(nn.LeakyReLU(0.2, true))
+-- state size: (ndf*2) x 16 x 16
+netD:add(SpatialConvolution(ndf * 2, ndf * 4, 4, 4, 2, 2, 1, 1))
+netD:add(SpatialBatchNormalization(ndf * 4)):add(nn.LeakyReLU(0.2, true))
+-- state size: (ndf*4) x 8 x 8
+netD:add(SpatialConvolution(ndf * 4, ndf * 8, 4, 4, 2, 2, 1, 1))
+netD:add(SpatialBatchNormalization(ndf * 8)):add(nn.LeakyReLU(0.2, true))
+-- state size: (ndf*8) x 4 x 4
+netD:add(SpatialConvolution(ndf * 8, 1, 4, 4))
+netD:add(nn.Sigmoid())
+-- state size: 1 x 1 x 1
+netD:add(nn.View(1):setNumInputDims(3))
+-- state size: 1
+
+netD:apply(weights_init)
+
+print ("netD end")
